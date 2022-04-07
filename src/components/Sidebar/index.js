ce15cc92f6fc805e4d8d39c22ba9288a4f255d40
EymanEP <ayenaxx@gmail.com>
2022-04-07 21:04:15 +0200

    Update index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,10 @@
 import './index.scss';
+import LogoS from './../../assets/images/logo-s.png';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className="logo" to="/">
-            <img src={}
+            <img src={LogoS} alt="logo" />
         </Link>
     </div>
 )
