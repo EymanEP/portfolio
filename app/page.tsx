51970eb63c8bd9b79d238e5d9bee3a0eb5ef46a0
Eyman <epashaliev02@gmail.com>
2024-10-24 23:47:57 +0200

    feat: work in progress page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import React from "react";
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <h1>Work in Progress</h1>
+        <div className="flex flex-col justify-center items-center h-dvh w-dvw">
+            <div className="w-3/5 self-center">
+                <Image src="/under-construction.png" className="w-full h-full object-cover"
+                     alt="illustration of people working"/>
+            </div>
+            <h1 className="text-3xl font-bold text-slate-700">This site is under construction</h1>
+            <p className="text-slate-500">This site will be available soon.</p>
+        </div>
     );
 }
