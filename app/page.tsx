7db20aff28792827db06c21144788b237ac20228
Eyman <epashaliev02@gmail.com>
2024-10-24 23:49:02 +0200

    fix: not going to use Image yet.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Image from "next/image";
 
 export default function Home() {
     return (
         <div className="flex flex-col justify-center items-center h-dvh w-dvw">
             <div className="w-3/5 self-center">
-                <Image src="/under-construction.png" className="w-full h-full object-cover"
-                     alt="illustration of people working"/>
+                <img src="/under-construction.png" className="w-full h-full object-cover"
+                     alt="illustration of people working" />
             </div>
             <h1 className="text-3xl font-bold text-slate-700">This site is under construction</h1>
             <p className="text-slate-500">This site will be available soon.</p>
