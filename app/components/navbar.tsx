f34c92eda6842dd9ad1617237fe01ae37c48d71b
Eyman <epashaliev02@gmail.com>
2024-10-26 15:12:10 +0200

    feat: renamed button to EffectButton.tsx and added animation

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,11 @@
-import Button from "@/app/components/atoms/Button";
+import EffectButton from "@/app/components/atoms/EffectButton";
 
 const Navbar = () => {
     return (<nav
         className={`fixed top-0 left-0 right-0 z-50 backdrop-blur-md shadow-md px-5 py-2 font-jetbrainsMono
              flex justify-between`}
     >
-        <Button>EP</Button>
+        <EffectButton>EP</EffectButton>
     </nav>)
 }
 
